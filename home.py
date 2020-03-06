7b9318117de6ec0ce6483ecfe42e13de8040b5d5
JceeJaxon <61695692+JceeJaxon@users.noreply.github.com>
2020-03-06 16:45:11 +0300

    Add files via upload

--- home.py
+++ home.py
@@ -1,1 +1,8 @@
-print('My name is Jcee')
+apple_price = 2
+money = 10
+input_count = input('How many apples do you want:\n')
+count = int(input_count)
+total_price = apple_price * count
+
+print('You will buy ' +str(count)+ ' apples')
+print('The total price will be ' +str(total_price)+ ' dollars')
